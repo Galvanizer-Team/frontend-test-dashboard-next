8eaa443458109cb4d84ced14b542ab2c4d408589
David Crammer <dcrammer777@gmail.com>
2023-10-17 16:21:22 -0700

    Update image

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <main className={`p-20 w-[600px] ${inter.className}`}>
       <h1 className="text-4xl">Hello There.</h1>
-      <p className="text-sm mt-1 text-zinc-700">This is a frontend application used to test some microservices.</p>
+      <div className="text-sm mt-1 text-zinc-700">
+        <p>This is a frontend application used to test some microservices.</p>
+        <p>This is version 1.0.5 of this application</p>
+      </div>
     </main>
   )
 }
